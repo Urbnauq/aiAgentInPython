783594c5ece88b49bf02ff651bae74c0ed78b3e0
urbnauq <eduardoalmonte18@gmai.com>
2025-07-07 21:31:04 -0400

    CH2:L1

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import os
 import sys
+
 from dotenv import load_dotenv
+
 from google import genai
+from google.genai import types
 
 def main():
-    # print("Hello from aiagentinpython!")
+    # Flags
+    flags = {
+         "verbose":"verbose",
+    }
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    # contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     if len(sys.argv) > 1 and sys.argv[1] != " ": 
-            contents = str(" ".join(sys.argv[1:]))
+            prompt = " ".join(sys.argv[1:]).split("--")
+            user_prompt = prompt[0]
     else:
         print("Provide a prompt")
         sys.exit(1)
     
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=contents)
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+         model="gemini-2.0-flash-001", 
+         contents=messages, 
+         )
+
     X = response.usage_metadata.prompt_token_count
     Y = response.usage_metadata.candidates_token_count
     
-    print(response.text)
-    print(f"Prompt tokens: {X}")
-    print(f"Response tokens: {Y}")
+    print(f"Response: {response.text}")
+    
+    if len(prompt) > 1 and flags.get(prompt[1]) == "verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {X}")
+        print(f"Response tokens: {Y}")
 
 if __name__ == "__main__":
     main()
-
-
